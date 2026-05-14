f9163760ad5e023fd9b13e0484c0fc7af78bf6e1
Shakirov-11309 <ilham_sh2005@mail.ru>
2026-05-14 17:34:35 +0300

    add Repository and DTOs
    добавлен репозиторий уведомлений  с работой бд. Созданы дто сущности и его конфигурации. Перенес контроллер c program.cs в соответствующий класс

---
+++ src/NotificationService/NotificationService.Api/Controllers/NotificationsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NotificationService.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotificationsController : Controller
+    {
+        [HttpGet("healthCheck")]
+        public async Task<IActionResult> GetHealthCheck()
+        {
+            return Ok("service = NotifactionsService\n" +
+                "status = Healthy\n" +
+                $"timestamp = {DateTime.UtcNow}");
+        }
+    }
+}
