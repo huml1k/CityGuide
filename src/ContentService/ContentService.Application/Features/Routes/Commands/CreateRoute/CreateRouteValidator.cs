f96fff8f1fc2c5692eef9803595d32d6e9821131
AlinaSadykova <144334653+Sadykova-11309@users.noreply.github.com>
2026-05-15 17:11:30 +0300

    CreateRouteHandler added

--- src/ContentService/ContentService.Application/Features/Routes/Commands/CreateRoute/CreateRouteValidator.cs
+++ src/ContentService/ContentService.Application/Features/Routes/Commands/CreateRoute/CreateRouteValidator.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ContentService.Application.Features.Routes.Commands.CreateRoute
 {
-    internal class CreateRouteValidator
+    public class CreateRouteValidator : AbstractValidator<CreateRouteCommand>
     {
+        public CreateRouteValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .MaximumLength(200);
+
+            RuleFor(x => x.Description)
+                .MaximumLength(2000);
+
+            RuleFor(x => x.DurationMinutes)
+                .GreaterThan(0);
+
+            RuleFor(x => x.Price)
+                .GreaterThanOrEqualTo(0);
+
+            RuleFor(x => x.CreatorId)
+                .NotEmpty();
+        }
     }
 }
