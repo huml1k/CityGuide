16cfa1a062506293627bcecfb93bc47a7b415ba9
AlinaSadykova <144334653+Sadykova-11309@users.noreply.github.com>
2026-05-15 17:31:26 +0300

    GetRouteByIdHandler added

--- src/ContentService/ContentService.Application/Features/Routes/Queries/GetRouteById/GetRouteByIdResponse.cs
+++ src/ContentService/ContentService.Application/Features/Routes/Queries/GetRouteById/GetRouteByIdResponse.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using ContentService.Application.DTOs;
+using ContentService.Application.Features.Routes.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ContentService.Application.Features.Routes.Queries.GetRouteById
 {
-    internal class GetRouteByIdResponse
+    public class GetRouteByIdResponse
     {
+        public Guid Id { get; set; }
+
+        public Guid CreatorId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public int DurationMinutes { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Status { get; set; } = string.Empty;
+
+        public string? GoogleMapsUrl { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public List<RoutePointDto> Points { get; set; } = [];
+
+        public List<RouteImageDto> Images { get; set; } = [];
     }
 }
