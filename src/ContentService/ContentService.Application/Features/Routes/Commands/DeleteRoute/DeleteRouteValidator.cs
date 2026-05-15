9538e58be7d05b1d175ee94273282e01d3363830
AlinaSadykova <144334653+Sadykova-11309@users.noreply.github.com>
2026-05-15 17:20:42 +0300

    DeleteRouteHandler added

--- src/ContentService/ContentService.Application/Features/Routes/Commands/DeleteRoute/DeleteRouteValidator.cs
+++ src/ContentService/ContentService.Application/Features/Routes/Commands/DeleteRoute/DeleteRouteValidator.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ContentService.Application.Features.Routes.Commands.DeleteRoute
 {
-    internal class DeleteRouteValidator
+    public class DeleteRouteValidator : AbstractValidator<DeleteRouteCommand>
     {
+        public DeleteRouteValidator()
+        {
+            RuleFor(x => x.RouteId)
+                .NotEmpty();
+        }
     }
 }
