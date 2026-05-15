936d2cb05a1ac038b5d8211839c38b40a13532bf
FayzullinTR <timur.faizullin@social-card.ru>
2026-05-16 00:49:14 +0300

    add entities for dbcontext

---
+++ src/AuthService/AuthService.Domain/Entities/User.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuthService.Domain.Entities
+{
+    [Table("users")]
+    public class User
+    {
+        [Key]
+        [Column("id")]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Column("email")]
+        [MaxLength(255)]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Column("password_hash")]
+        [MaxLength(512)]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [Column("role")]
+        [MaxLength(50)]
+        public string Role { get; set; } = "User";
+
+        [Column("is_email_confirmed")]
+        public bool IsEmailConfirmed { get; set; } = false;
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("updated_at")]
+        public DateTime? UpdatedAt { get; set; }
+
+        [Column("deleted_at")]
+        public DateTime? DeletedAt { get; set; }
+    }
+}
---
+++ src/AuthService/AuthService.Domain/Entities/UserSession.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuthService.Domain.Entities
+{
+    [Table("user_sessions")]
+    public class UserSession
+    {
+        [Key]
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Column("user_id")]
+        public Guid UserId { get; set; }
+
+        [Required]
+        [Column("refresh_token_hash")]
+        [MaxLength(255)]
+        public string RefreshTokenHash { get; set; } = string.Empty;
+
+        [Required]
+        [Column("expires_at")]
+        public DateTime ExpiresAt { get; set; }
+
+        [Required]
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("revoked_at")]
+        public DateTime? RevokedAt { get; set; }
+
+        [MaxLength(255)]
+        [Column("user_agent")]
+        public string? UserAgent { get; set; }
+
+        [MaxLength(45)]
+        [Column("ip_address")]
+        public string? IpAddress { get; set; }
+
+    }
+}
