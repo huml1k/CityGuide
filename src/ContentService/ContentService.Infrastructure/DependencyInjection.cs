57e9c460156212b20a3b9414afa6b888d9fa9648
AlinaSadykova <144334653+Sadykova-11309@users.noreply.github.com>
2026-05-16 17:55:21 +0300

    DI update

--- src/ContentService/ContentService.Infrastructure/DependencyInjection.cs
+++ src/ContentService/ContentService.Infrastructure/DependencyInjection.cs
@@ -13,13 +13,15 @@ namespace ContentService.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(
-        this IServiceCollection services,
-        IConfiguration configuration)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<ContentDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 
             services.AddScoped<IRouteRepository, RouteRepository>();
+
+            services.AddScoped<IAudioFileRepository, AudioFileRepository>();
+
+            services.AddScoped<IRouteImageRepository, RouteImageRepository>();
 
             services.AddScoped<ITagRepository, TagRepository>();
 
