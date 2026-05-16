d46cdbe16d43a262d68a91a2ceaa2b894a9d0a9c
ParZival <68954480+ParZIvalXL@users.noreply.github.com>
2026-05-17 02:26:42 +0300

    Profile Service added

---
+++ src/UserService/UserService.Domain/Interfaces/Service/IUserProfileService.cs
@@ -0,0 +1,11 @@
+﻿namespace UserService.Domain.Interfaces.Service;
+
+
+public interface IUserProfileService
+{
+    Task<UserProfileDto> GetMyProfileAsync(CancellationToken ct = default);
+    Task<UserProfileDto> GetByIdAsync(Guid userId, CancellationToken ct = default);
+    Task<Guid> CreateMyProfileAsync(CreateUserProfileRequest request, CancellationToken ct = default);
+    Task UpdateMyProfileAsync(UpdateUserProfileRequest request, CancellationToken ct = default);
+    Task DeleteMyProfileAsync(CancellationToken ct = default);
+}
