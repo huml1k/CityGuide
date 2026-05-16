6d87459d23d7c0f6bdf1707683164837e6ad1476
AlinaSadykova <144334653+Sadykova-11309@users.noreply.github.com>
2026-05-16 17:55:11 +0300

    Controllers added

--- src/ContentService/ContentService.Api/Controllers/TagsController.cs
+++ src/ContentService/ContentService.Api/Controllers/TagsController.cs
@@ -1,83 +1,30 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ContentService.Application.Features.Tags.Queries.GetTags;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContentService.Api.Controllers
 {
+    [ApiController]
+    [Route("api/tags")]
     public class TagsController : Controller
     {
-        // GET: TagsController
-        public ActionResult Index()
+        private readonly IMediator _mediator;
+
+        public TagsController(IMediator mediator)
         {
-            return View();
+            _mediator = mediator;
         }
 
-        // GET: TagsController/Details/5
-        public ActionResult Details(int id)
+        //get all
+        [HttpGet]
+        public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
         {
-            return View();
-        }
+            var query = new GetTagsQuery();
 
-        // GET: TagsController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+            var result = await _mediator.Send(query, cancellationToken);
 
-        // POST: TagsController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: TagsController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: TagsController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: TagsController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: TagsController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return Ok(result);
         }
     }
 }
