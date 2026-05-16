bbebb5eb60c3c983af9ad4c13d10b447379802fd
AlinaSadykova <144334653+Sadykova-11309@users.noreply.github.com>
2026-05-16 14:43:37 +0300

    GetRoutesHandler update

--- src/ContentService/ContentService.Application/Features/Routes/Queries/GetRoutes/GetRoutesHandler.cs
+++ src/ContentService/ContentService.Application/Features/Routes/Queries/GetRoutes/GetRoutesHandler.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using ContentService.Domain.Interfaces.Repositories;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ContentService.Application.Features.Routes.Queries.GetRoutes
 {
-    internal class GetRoutesHandler
+    public class GetRoutesHandler : IRequestHandler<GetRoutesQuery,IReadOnlyCollection<GetRoutesResponse>>
     {
+        private readonly IRouteRepository _routeRepository;
+        public GetRoutesHandler(IRouteRepository routeRepository)
+        {
+            _routeRepository = routeRepository;
+        }
+
+        public async Task<IReadOnlyCollection<GetRoutesResponse>> Handle(GetRoutesQuery request, CancellationToken cancellationToken)
+        {
+            var routes = await _routeRepository.GetAllAsync(cancellationToken);
+
+            return routes
+                .Select(route => new GetRoutesResponse
+                {
+                    Id = route.Id,
+                    Title = route.Title,
+                    Description = route.Description,
+                    DurationMinutes = route.DurationMinutes,
+                    FavoritesCount = route.RouteStats.FavoritesCount,
+                    CoverImageExtension = route.RouteImages
+                        .FirstOrDefault(x => x.IsCover)?
+                        .FileExtension
+                })
+                .ToList();
+        }
     }
 }
