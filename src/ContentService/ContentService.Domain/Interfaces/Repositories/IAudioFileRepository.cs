b419d49d413112f108f435d435e794f1c3255a53
AlinaSadykova <144334653+Sadykova-11309@users.noreply.github.com>
2026-05-16 17:02:27 +0300

    Audio and Image repositories added

---
+++ src/ContentService/ContentService.Domain/Interfaces/Repositories/IAudioFileRepository.cs
@@ -0,0 +1,30 @@
+﻿using ContentService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContentService.Domain.Interfaces.Repositories
+{
+    public interface IAudioFileRepository
+    {
+        /// <summary>
+        /// Загрузка метаданных
+        /// </summary>
+        Task AddAsync(AudioFile audioFile, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Для скачивания/удаления файла
+        /// </summary>
+        Task<AudioFile?> GetByIdAsync( Guid id, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Получение всех файлов маршрута
+        /// </summary>
+        Task<IReadOnlyCollection<AudioFile>> GetByRouteIdAsync(Guid routeId, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Удаление метаданных
+        /// </summary>
+        void Delete(AudioFile audioFile);
+    }
+}
