15918d3d8ab9ca0337d877d0523bfad89187e44b
Shakirov-11309 <ilham_sh2005@mail.ru>
2026-05-17 15:17:35 +0300

    Refactor code
    отрефакторен код для соблюдения чистый архитектуры

--- src/NotificationService/NotificationService.Api/Program.cs
+++ src/NotificationService/NotificationService.Api/Program.cs
@@ -1,8 +1,10 @@
 using Confluent.Kafka;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using NotificationService.Application;
 using NotificationService.Application.Services;
 using NotificationService.Application.Services.Interface;
+using NotificationService.Infrastructure;
 using NotificationService.Infrastructure.Consumers;
 using NotificationService.Infrastructure.Persistence;
 using NotificationService.Infrastructure.Repository;
@@ -17,29 +19,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.Configure<ConsumerConfig>(cfg =>
-{
-    cfg.BootstrapServers = builder.Configuration["Kafka:BootstrapServers"] ?? "kafka:9092";
-});
-builder.Services.AddSingleton(sp => sp.GetRequiredService<IOptions<ConsumerConfig>>().Value);
-
-builder.Services.Configure<ProducerConfig>(cfg =>
-{
-    cfg.BootstrapServers = builder.Configuration["Kafka:BootstrapServers"] ?? "kafka:9092";
-});
-builder.Services.AddSingleton(sp => sp.GetRequiredService<IOptions<ProducerConfig>>().Value);
-
-
-builder.Services.AddSingleton<IDeadLetterQueueService, KafkaDlqService>();
-builder.Services.AddSingleton<IEventNotificationFactory, EventNotificationFactory>();
-builder.Services.AddScoped<INotificationRepository, NotificationRepository>();
+builder.Services.AddNotificationApplication();
+builder.Services.AddNotificationInfrastructure(builder.Configuration);
 
 // === EF Core ===
 builder.Services.AddDbContext<NotificationDbContext>(options =>
     options.UseNpgsql(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddHostedService<PushNotificationKafkaConsumer>();
 
 var app = builder.Build();
 
