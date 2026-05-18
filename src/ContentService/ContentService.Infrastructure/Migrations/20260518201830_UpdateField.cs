692f2bfdb4f8c29923c6c801ca6de687742c0d14
FayzullinTR <timur.faizullin@social-card.ru>
2026-05-18 23:18:38 +0300

    update migrations

---
+++ src/ContentService/ContentService.Infrastructure/Migrations/20260518201830_UpdateField.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContentService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "path",
+                table: "route_images",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "path",
+                table: "audio_files",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "path",
+                table: "route_images");
+
+            migrationBuilder.DropColumn(
+                name: "path",
+                table: "audio_files");
+        }
+    }
+}
