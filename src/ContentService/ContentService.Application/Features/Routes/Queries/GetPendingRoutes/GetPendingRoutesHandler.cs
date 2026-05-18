e69430e1786a0902174746ee0436969bc49f23d4
AlinaSadykova <144334653+Sadykova-11309@users.noreply.github.com>
2026-05-18 15:57:39 +0300

    Queries for moderation added

---
+++ src/ContentService/ContentService.Application/Features/Routes/Queries/GetPendingRoutes/GetPendingRoutesHandler.cs
@@ -0,0 +1,43 @@
+﻿using ContentService.Domain.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContentService.Application.Features.Routes.Queries.GetPendingRoutes
+{
+    public class GetPendingRoutesHandler : IRequestHandler<GetPendingRoutesQuery,IReadOnlyCollection<GetPendingRoutesResponse>>
+    {
+        private readonly IRouteRepository _routeRepository;
+        public GetPendingRoutesHandler(IRouteRepository routeRepository)
+        {
+            _routeRepository = routeRepository;
+        }
+
+        public async Task<IReadOnlyCollection<GetPendingRoutesResponse>> Handle(GetPendingRoutesQuery request, CancellationToken cancellationToken)
+        {
+            var routes = await _routeRepository.GetPendingModerationAsync(cancellationToken);
+
+            return routes
+                .Select(route => new GetPendingRoutesResponse
+                {
+                    Id = route.Id,
+                    Title = route.Title,
+                    Description = route.Description,
+                    DurationMinutes = route.DurationMinutes,
+
+                    FavoritesCount =
+                        route.RouteStats?.FavoritesCount ?? 0,
+
+                    CoverImageExtension = route.RouteImages
+                        .FirstOrDefault(x => x.IsCover)?
+                        .FileExtension,
+
+                    Tags = route.RouteTags
+                        .Select(x => x.Tag.Name)
+                        .ToList()
+                })
+                .ToList();
+        }
+    }
+}
