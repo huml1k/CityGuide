40edcaf330bb27fcea691b05dadf4b68596ea594
AlinaSadykova <144334653+Sadykova-11309@users.noreply.github.com>
2026-05-18 15:57:19 +0300

    Reject Apprrove Command added

---
+++ src/ContentService/ContentService.Application/Features/Routes/Commands/ApproveRoute/ApproveRouteValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContentService.Application.Features.Routes.Commands.ApproveRoute
+{
+    public class ApproveRouteValidator : AbstractValidator<ApproveRouteCommand>
+    {
+        public ApproveRouteValidator()
+        {
+            RuleFor(x => x.RouteId)
+                .NotEmpty();
+        }
+    }
+}
