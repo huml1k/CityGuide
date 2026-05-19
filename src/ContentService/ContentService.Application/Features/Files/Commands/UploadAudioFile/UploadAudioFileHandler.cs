80e3f90b9f97648f418b344ffa9b499abe9fbfef
ParZival <68954480+ParZIvalXL@users.noreply.github.com>
2026-05-19 16:57:58 +0300

    Added check for existance for Route

--- src/ContentService/ContentService.Application/Features/Files/Commands/UploadAudioFile/UploadAudioFileHandler.cs
+++ src/ContentService/ContentService.Application/Features/Files/Commands/UploadAudioFile/UploadAudioFileHandler.cs
@@ -5,21 +5,26 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using ContentService.Application.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace ContentService.Application.Features.Files.Commands.UploadAudioFile
 {
     public class UploadAudioFileCommandHandler : IRequestHandler<UploadAudioFileCommand, UploadAudioFileResponse>
     {
         private readonly IAudioFileRepository _audioRepository;
+        private readonly IRouteRepository _routeRepository;
         private readonly IUnitOfWork _unitOfWork;
         private readonly IFileStorageService _fileStorageService;
+        private readonly ILogger<UploadAudioFileCommandHandler> _logger;
         private readonly string _bucketName = "content-files";
 
-        public UploadAudioFileCommandHandler(IAudioFileRepository audioRepository, IUnitOfWork unitOfWork, IFileStorageService fileStorageService)
+        public UploadAudioFileCommandHandler(IAudioFileRepository audioRepository, IUnitOfWork unitOfWork, IFileStorageService fileStorageService, IRouteRepository  routeRepository, ILogger<UploadAudioFileCommandHandler> logger)
         {
             _audioRepository = audioRepository;
             _unitOfWork = unitOfWork;
             _fileStorageService = fileStorageService;
+            _routeRepository = routeRepository;
+            _logger = logger;
         }
 
         public async Task<UploadAudioFileResponse> Handle(UploadAudioFileCommand request, CancellationToken cancellationToken)
@@ -28,6 +33,11 @@ namespace ContentService.Application.Features.Files.Commands.UploadAudioFile
                 .GetExtension(request.File.FileName)
                 .Replace(".", "");
             
+            if (!await _routeRepository.ExistsAsync(request.RouteId, cancellationToken))
+            {
+                _logger.LogError($"ERROR: Route with id: {request.RouteId} does not exist in DB");
+                throw new Exception($"ERROR: Route with id: {request.RouteId} does not exist in DB");
+            }
             await _fileStorageService.UploadFileAsync(request.File.OpenReadStream(), request.File.Name, extension, cancellationToken);
             var filePath = _bucketName + "/" + request.File.FileName;
             
--- src/ContentService/ContentService.Application/Features/Files/Commands/UploadRouteImage/UploadRouteImageHandler.cs
+++ src/ContentService/ContentService.Application/Features/Files/Commands/UploadRouteImage/UploadRouteImageHandler.cs
@@ -5,24 +5,31 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using ContentService.Application.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace ContentService.Application.Features.Files.Commands.UploadRouteImage
 {
     public class UploadRouteImageCommandHandler : IRequestHandler<UploadRouteImageCommand, UploadRouteImageResponse>
     {
         private readonly IRouteImageRepository _imageRepository;
+        private readonly IRouteRepository _routeRepository;
         private readonly IFileStorageService _fileStorageService;
         private readonly IUnitOfWork _unitOfWork;
         private readonly string _bucketName = "content-files";
+        private readonly ILogger<UploadRouteImageCommandHandler> _logger;
 
         public UploadRouteImageCommandHandler(
             IRouteImageRepository imageRepository,
             IUnitOfWork unitOfWork,
-            IFileStorageService fileStorageService)
+            IFileStorageService fileStorageService,
+            ILogger<UploadRouteImageCommandHandler> logger,
+            IRouteRepository routeRepository)
         {
             _imageRepository = imageRepository;
             _unitOfWork = unitOfWork;
             _fileStorageService = fileStorageService;
+            _logger = logger;
+            _routeRepository = routeRepository;
         }
 
         public async Task<UploadRouteImageResponse> Handle(UploadRouteImageCommand request, CancellationToken cancellationToken)
@@ -30,6 +37,12 @@ namespace ContentService.Application.Features.Files.Commands.UploadRouteImage
             var extension = Path
                 .GetExtension(request.File.FileName)
                 .Replace(".", "");
+            
+            if (!await _routeRepository.ExistsAsync(request.RouteId, cancellationToken))
+            {
+                _logger.LogError($"ERROR: Route with id: {request.RouteId} does not exist in DB");
+                throw new Exception($"ERROR: Route with id: {request.RouteId} does not exist in DB");
+            }
             
             await _fileStorageService.UploadFileAsync(request.File.OpenReadStream(), request.File.Name, extension, cancellationToken);
             var filePath = _bucketName + "/" + request.File.FileName;
