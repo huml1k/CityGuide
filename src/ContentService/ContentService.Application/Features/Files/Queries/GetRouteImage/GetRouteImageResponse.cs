77d8bd729b0bb66ae9e27142108f3c1a5cc16383
ParZival <68954480+ParZIvalXL@users.noreply.github.com>
2026-05-19 19:19:41 +0300

    Response changed from file stream to url

--- src/ContentService/ContentService.Application/Features/Files/Queries/GetRouteImage/GetRouteImageResponse.cs
+++ src/ContentService/ContentService.Application/Features/Files/Queries/GetRouteImage/GetRouteImageResponse.cs
@@ -6,10 +6,9 @@ namespace ContentService.Application.Features.Files.Queries.GetRouteImage
 {
     public class GetRouteImageResponse
     {
-        public Stream Stream { get; set; } = null!;
-
-        public string ContentType { get; set; } = string.Empty;
-
+        public string Url { get; set; } = string.Empty;
+        public DateTime ExpiresAt { get; set; }
         public string FileName { get; set; } = string.Empty;
+        public string ContentType { get; set; } = string.Empty;
     }
 }
