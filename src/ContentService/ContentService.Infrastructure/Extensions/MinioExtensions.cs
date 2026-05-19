22ed294cf27dd79e545d15366a22304db5b6b208
ParZival <68954480+ParZIvalXL@users.noreply.github.com>
2026-05-19 16:59:10 +0300

    Created extension for MinIo

--- src/ContentService/ContentService.Infrastructure/Extensions/MinioExtensions.cs
+++ src/ContentService/ContentService.Infrastructure/Extensions/MinioExtensions.cs
@@ -1,10 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ContentService.Application.Interfaces;
+using ContentService.Infrastructure.MinIo;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Minio;
 
 namespace ContentService.Infrastructure.Extensions
 {
-    internal class MinioExtensions
+    public static class MinioExtensions
     {
+        public static IServiceCollection AddMinIoStorage(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddOptions<MinIoOptions>()
+                .Bind(configuration.GetSection(MinIoOptions.SectionName))
+                .Validate(o => !string.IsNullOrWhiteSpace(o.Endpoint), "MinIo:Endpoint is required")
+                .Validate(o => !string.IsNullOrWhiteSpace(o.ContentBucket), "MinIo:ContentBucket is required")
+                .ValidateOnStart();
+
+            services.AddSingleton<IMinioClient>(sp =>
+            {
+                var options = sp.GetRequiredService<IOptions<MinIoOptions>>().Value;
+                var builder = new MinioClient()
+                    .WithEndpoint(options.Endpoint)
+                    .WithCredentials(options.AccessKey, options.SecretKey);
+                if (options.UseSsl)
+                    builder = builder.WithSSL();
+                return builder.Build();
+            });
+
+            services.AddScoped<IFileStorageService, MinIoFileStorage>();
+            return services;
+        }
     }
 }
